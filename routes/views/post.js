5336fccf947156b803ea67b21d4f71fe6bc8644a
Jonathan <jonathanzejunhu@gmail.com>
2018-08-24 17:07:28 -0400

    Added single post page

--- routes/views/post.js
+++ routes/views/post.js
@@ -20,7 +20,7 @@ exports = module.exports = function (req, res) {
 		var q = keystone.list('Post').model.findOne({
 			state: 'published',
 			slug: locals.filters.post,
-		}).populate('author categories');
+		}).populate('author');
 
 		q.exec(function (err, result) {
 			locals.data.post = result;
@@ -29,17 +29,7 @@ exports = module.exports = function (req, res) {
 
 	});
 
-	// Load other posts
-	view.on('init', function (next) {
 
-		var q = keystone.list('Post').model.find().where('state', 'published').sort('-publishedDate').populate('author').limit('4');
-
-		q.exec(function (err, results) {
-			locals.data.posts = results;
-			next(err);
-		});
-
-	});
 
 	// Render the view
 	view.render('post');
