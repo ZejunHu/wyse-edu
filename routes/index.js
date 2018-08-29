15c767f6779b50888e46752f1e0645e9ec07e377
Jonathan <jonathanzejunhu@gmail.com>
2018-08-29 13:51:05 -0400

    Completed camp and contact page

--- routes/index.js
+++ routes/index.js
@@ -54,6 +54,8 @@ exports = module.exports = function (app) {
 
 	app.all('/visa', routes.views.visa);
 
+	app.all('/camp', routes.views.camp);
+
 	app.all('/contact', routes.views.contact);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
